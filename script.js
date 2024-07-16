262b526a5efb3ea1a44b8f9f01a3ccb3afc28251
Mohit Jaiswal <mohitjaiswal2883@gmail.com>
2024-07-16 14:25:22 +0530

    u

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 // Author - Mohit Jaiswal
 
@@ -33,39 +32,3 @@ item.addEventListener('click', function() {
   item.classList.add('active');
 });
 });
-=======
-
-// Author - Mohit Jaiswal
-
-
-// When you refresh/reload -> goes to home page
-window.onload = function() {
-  window.setTimeout(
-      function() { window.scrollTo(0,0); },
-      10
-  );
-};
-
-
-// Navbar hamburger toggle closes when you select an option
-$('.navbar-nav>li>a').on('click', function(){
-  $('.navbar-collapse').collapse('hide');
-});
-
-
-// AOS
-AOS.init();
-
-
-// Naviagtion Bar Settings
-var navbarItems = document.querySelectorAll('.navbar-nav .nav-link');
-
-navbarItems.forEach(function(item) {
-item.addEventListener('click', function() {
-  navbarItems.forEach(function(navbarItem) {
-    navbarItem.classList.remove('active');
-  });
-  item.classList.add('active');
-});
-});
->>>>>>> origin/main
